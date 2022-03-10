a99703a0084f5ccf48c12a2bbc186aabe065d489
Sonu Dutta <itsmesonudutta@gmail.com>
2022-03-10 10:58:09 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let textbox=document.getElementById("textbox");
+textbox.addEventListener('input',function(){
+    var text=this.value;
+    let char=text.length;
+    document.getElementById("char").innerHTML=char;
+
+    text=text.trim();
+
+    let words=text.split(" ");
+    let cleanList=words.filter(function(elm){
+        return elm != "";
+    });
+    console.log(cleanList);
+    document.getElementById('word').innerHTML=cleanList.length;
+});
